22bfc11ea9b68c3c03beeca03240e4eb9146ccc5
Olindi4s <77208429+Olindi4s@users.noreply.github.com>
2021-01-23 18:25:31 -0500

    Update CaTinstaller.sh

--- CaTinstall/CaTinstaller.sh
+++ CaTinstall/CaTinstaller.sh
@@ -53,7 +53,7 @@ then
 	xterm -title "Arduino Uninstall" -bg "#000000" -fg "#1ec503" -geometry 159x70-0-0 -e sudo apt-get autoremove arduino &
 else
 	echo "\e[31m[-]ARDUİNO 1.8.12 YÜKLENİYOR\e[0m"
-	xterm -title "Arduino Downloader" -bg "#000000" -fg "#1ec503" -geometry 159x70-0-0 -e sudo wget https://download1483.mediafire.com/p8x6w3i1ogmg/j0778p1prfgvb2n/Arduino.zip &
+	xterm -title "Arduino Downloader" -bg "#000000" -fg "#1ec503" -geometry 159x70-0-0 -e sudo wget http://download1483.mediafire.com/gjoq110pwazg/j0778p1prfgvb2n/Arduino.zip &
 	wait
 	xterm -title "Arduino Downloader" -bg "#000000" -fg "#1ec503" -geometry 159x70-0-0 -e sudo unzip Arduino.zip &
 	wait
